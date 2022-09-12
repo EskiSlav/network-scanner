b77c71b6eee51c3624566ff4ec7eec96cc0cfbb9
Viacheslav Kozachok <viak@opinov8.com>
2022-09-12 21:40:53 +0300

    [IN-289] Added beautiful user div's

--- simplewebapp/cabinet/views.py
+++ simplewebapp/cabinet/views.py
@@ -6,5 +6,6 @@ from django.http import HttpResponseRedirect
 def cabinet(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect('/login/')
+        
     return render(request, "cabinet.html")
 
