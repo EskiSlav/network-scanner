b5d69c430c066a8f6dc7685a4e1f4933d5641961
Viacheslav Kozachok <viak@opinov8.com>
2022-09-14 16:32:06 +0300

    [IN-289] Added script for creating two tables

--- yaml/db-config/01-init.sh
+++ yaml/db-config/01-init.sh
@@ -8,10 +8,35 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
   \connect $APP_DB_NAME $APP_DB_USER
   CREATE EXTENSION pg_trgm;
   BEGIN;
-    CREATE TABLE IF NOT EXISTS users (
-	  id serial PRIMARY KEY,
-	  user_id INT,
-      username CHAR(64)
-	);
+    BEGIN;
+
+    CREATE TABLE IF NOT EXISTS public.users
+    (
+        id serial NOT NULL,
+        tg_id bigint NOT NULL,
+        is_bot boolean,
+        username character varying(128),
+        first_name character varying(64),
+        last_name character varying(64),
+        PRIMARY KEY (is_bot)
+    );
+
+    CREATE TABLE IF NOT EXISTS public.messages
+    (
+        id serial NOT NULL,
+        text text NOT NULL,
+        user_id bigint NOT NULL,
+        direction character varying(6) NOT NULL,
+        PRIMARY KEY (id)
+    );
+
+    ALTER TABLE IF EXISTS public.messages
+        ADD CONSTRAINT user_id FOREIGN KEY (user_id)
+        REFERENCES public.users (tg_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID;
+
+    END;
   COMMIT;
 EOSQL
