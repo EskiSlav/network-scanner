2358e25a4ab4e08e77d8d6b6fbd03fcaae7c60c5
Viacheslav Kozachok <viak@opinov8.com>
2022-09-17 05:03:33 +0300

    [IN-289] Added response with messages from database by user_id

--- simplewebapp/cabinet/views.py
+++ simplewebapp/cabinet/views.py
@@ -1,6 +1,6 @@
 from multiprocessing import context
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 import psycopg2
 import os
 
@@ -22,6 +22,12 @@ class DB:
         self.cursor.execute(sql)
         return self.cursor.fetchall()
 
+    def get_data_for_messages_side(self, user_id):
+        sql = '''
+        SELECT user_id, text, direction, message_id FROM messages WHERE user_id=%s '''
+        self.cursor.execute(sql, (user_id,))
+        return self.cursor.fetchall()
+
     def close(self):
         self.connection.close()
 
@@ -36,5 +42,26 @@ def cabinet(request):
                         'username': user[1], 
                         'first_name': user[2], 
                         'last_name': user[3] } for user in user_data_list ]
+    db.close()
     return render(request, "cabinet.html", context={'users': user_data_list})
 
+def send_messages(request, user_id):
+    data = {
+        '0': [
+            #data added here
+        ]
+    }
+    # user_id = int(request.path.split('/')[-2])
+    db = DB()
+    messages_data_list = db.get_data_for_messages_side(user_id)
+    for message in messages_data_list:
+        data['0'].append(
+            {
+                'user_id': message[0],
+                'text': message[1],
+                'direction': message[2],
+                'message_id': message[3],
+            }
+        )
+    db.close()
+    return JsonResponse(data)
